302e3bc35d03dc1b7eb848e5f133f4bd9f822e2e
Nuwan Jaliyagoda <NuwanJ@users.noreply.github.com>
2024-01-15 16:55:30 +0530

    Announcement basics (#5)

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -13,6 +13,9 @@
     <link href="{{ mix('css/backend.css') }}" rel="stylesheet">
     <livewire:styles />
     @stack('after-styles')
+
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body class="c-app">
     @include('backend.includes.sidebar')
