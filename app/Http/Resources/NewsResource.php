226989d34ae4cfcaddc51b7e341066b68c4e49fe
IsharaEkanayaka <e20094@eng.pdn.ac.lk>
2024-07-02 21:43:25 +0530

    implement API gateway for news items

---
+++ app/Http/Resources/NewsResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NewsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'author' => $this->author,
+            'image' => $this->image,
+            'link_url' => $this->link_url,
+            'link_caption' => $this->link_caption,
+            'posted_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+
+        ];
+    }
+}
