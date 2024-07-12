fc7811363b049718ad49e6eb1840a9a1c30b4ab9
Hasan10100 <150885485+Hasan10100@users.noreply.github.com>
2024-07-12 17:26:26 +0530

    Edit placeholders to show previous values

--- resources/views/backend/event/edit.blade.php
+++ resources/views/backend/event/edit.blade.php
@@ -111,7 +111,8 @@
                 <div class="form-group row">
                     {!! Form::label('start_time', 'Start Time*', ['class' => 'col-md-2 col-form-label']) !!}
                     <div class="col-md-10">
-                        {!! Form::text('start_time', '', ['class' => 'form-control', 'id' => 'start_time', 'required' => true]) !!}
+                        {!! Form::text('start_time', $event->start_time, [
+                            'class' => 'form-control', 'id' => 'start_time', 'required' => true]) !!}
                         @error('start_time')
                             <strong>{{ $message }}</strong>
                         @enderror
@@ -122,7 +123,8 @@
                 <div class="form-group row">
                     {!! Form::label('end_time', 'End Time*', ['class' => 'col-md-2 col-form-label']) !!}
                     <div class="col-md-10">
-                        {!! Form::text('end_time', '', ['class' => 'form-control', 'id' => 'end_time', 'required' => true]) !!}
+                        {!! Form::text('end_time', $event->end_time, [
+                            'class' => 'form-control', 'id' => 'end_time', 'required' => true]) !!}
                         @error('end_time')
                             <strong>{{ $message }}</strong>
                         @enderror
@@ -134,12 +136,26 @@
                 {!! Form::label('location', 'Location*', ['class' => 'col-md-2 col-form-label']) !!}
 
                 <div class="col-md-10">
-                    {!! Form::text('location', '', ['class' => 'form-control', 'required' => true]) !!}
-                    @error('link_caption')
+                    {!! Form::text('location', $event->location, [
+                        'class' => 'form-control', 'required' => true]) !!}
+                    @error('location')
                         <strong>{{ $message }}</strong>
                     @enderror
                 </div>
             </div>
+
+            <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+            <script>
+                // Initialize flatpickr on the input field
+                flatpickr("#start_time", {
+                    enableTime: true,        // Enable time selection
+                    dateFormat: "Y-m-d H:i", // Set the date and time format
+                });
+                flatpickr("#end_time", {
+                    enableTime: true,        // Enable time selection
+                    dateFormat: "Y-m-d H:i", // Set the date and time format
+                });
+            </script>
 
             </x-slot>
             <x-slot name="footer">
