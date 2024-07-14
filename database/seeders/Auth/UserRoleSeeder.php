4e9a445c22ea6c8375d37bd032c9d7fdbe91a44c
IsharaEkanayaka <e20094@eng.pdn.ac.lk>
2024-07-14 20:00:14 +0530

    assign News Editor and Event Editor roles to Super Admin

--- database/seeders/Auth/UserRoleSeeder.php
+++ database/seeders/Auth/UserRoleSeeder.php
@@ -23,7 +23,7 @@ class UserRoleSeeder extends Seeder
         User::find(1)->assignRole(config('boilerplate.access.role.admin'));
         User::find(3)->assignRole('News Editor');
         User::find(4)->assignRole('Event Editor');
-
+        User::find(1)->assignRole('News Editor','Event Editor');
         $this->enableForeignKeys();
     }
 }
