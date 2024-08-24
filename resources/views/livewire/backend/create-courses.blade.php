f7364a58a12e2ffb386a6c0a7c08f6dc5df70417
Hasan10100 <150885485+Hasan10100@users.noreply.github.com>
2024-08-24 21:10:08 +0530

    Update create-courses.blade.php
    create dummy HTML for Basics page

--- resources/views/livewire/backend/create-courses.blade.php
+++ resources/views/livewire/backend/create-courses.blade.php
@@ -21,7 +21,234 @@
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Basics</h5>
-                        <p class="card-text">This is the first step of the form.</p>
+                        <div class="basics">
+                            <form action="#">
+
+                                <div class="row" id="row1">
+                                    <div class="col-6 col-xl-3 py-3">
+                                        <div class="col">
+                                            <label for="drop1">Academic Program</label>
+                                        </div>
+                                        <div class="dropdown-center">
+                                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Dropdown button
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 col-xl-3 py-3">
+                                        <div class="col">
+                                            <label for="drop1">Academic Program</label>
+                                        </div>
+                                        <div class="dropdown-center">
+                                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Dropdown button
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 col-xl-3 py-3">
+                                        <div class="col">
+                                            <label for="drop1">Academic Program</label>
+                                        </div>
+                                        <div class="dropdown-center">
+                                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Dropdown button
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 col-xl-3 py-3">
+                                        <div class="col">
+                                            <label for="drop1">Academic Program</label>
+                                        </div>
+                                        <div class="dropdown-center">
+                                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Dropdown button
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row" id="row2">
+                                    <div class="col-6 col-xl-3 py-3">
+                                        <div class="col">
+                                            <label for="drop1">Academic Program</label>
+                                        </div>
+                                        <div class="dropdown-center">
+                                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Dropdown button
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 col-xl-3 py-3">
+                                        <div class="col">
+                                            <label for="drop1">Academic Program</label>
+                                        </div>
+                                        <div class="dropdown-center">
+                                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Dropdown button
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 col-xl-3 py-3">
+                                        <div class="col">
+                                            <label for="drop1">Academic Program</label>
+                                        </div>
+                                        <div class="dropdown-center">
+                                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Dropdown button
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 col-xl-3 py-3">
+                                        <div class="col">
+                                            <label for="drop1">Academic Program</label>
+                                        </div>
+                                        <div class="dropdown-center">
+                                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Dropdown button
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="my-2" id="contentarea">
+                                    <label class="pb-2" for="textarea">Academic</label>
+                                    <div class="form-floating">
+                                        <textarea id="textarea" class="form-control auto-resize-textarea" id="floatingTextarea" style="height: 55px;" oninput="this.style.height = ''; this.style.height = this.scrollHeight + 'px'"></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="div my-2">
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <div class="div text-center">
+                                                <label for="drop1">Time Allocation</label>
+                                            </div>
+                                            <hr>
+                                            <div class="row pb-2">
+                                                <div class="row d-flex justify-content-between align-items-center col-md-6 ">
+                                                    <div class="col">
+                                                        <label>Lectures</label>
+                                                    </div>
+                                                    <div class="col ms-auto py-3">
+                                                        <input class="form-control" type="number">
+                                                    </div>
+                                                </div>
+                                                <div class="row d-flex justify-content-between align-items-center col-md-6 ">
+                                                    <div class="col">
+                                                        <label>Lectures</label>
+                                                    </div>
+                                                    <div class="col ms-auto py-3">
+                                                        <input class="form-control" type="number">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row pb-2">
+                                                <div class="row d-flex justify-content-between align-items-center col-md-6 ">
+                                                    <div class="col">
+                                                        <label>Lectures</label>
+                                                    </div>
+                                                    <div class="col ms-auto py-3">
+                                                        <input class="form-control" type="number">
+                                                    </div>
+                                                </div>
+                                                <div class="row d-flex justify-content-between align-items-center col-md-6 ">
+                                                    <div class="col">
+                                                        <label>Lectures</label>
+                                                    </div>
+                                                    <div class="col ms-auto py-3">
+                                                        <input class="form-control" type="number">
+                                                    </div>
+                                                </div>
+                                            </div>                     
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="div text-center">
+                                                <label for="drop1">Time Allocation</label>
+                                            </div>
+                                            <hr>
+                                            <div class="row pb-2">
+                                                <div class="row d-flex justify-content-between align-items-center col-md-6 ">
+                                                    <div class="col">
+                                                        <label>Lectures</label>
+                                                    </div>
+                                                    <div class="col ms-auto py-3">
+                                                        <input class="form-control" type="number">
+                                                    </div>
+                                                </div>
+                                                <div class="row d-flex justify-content-between align-items-center col-md-6 ">
+                                                    <div class="col">
+                                                        <label>Lectures</label>
+                                                    </div>
+                                                    <div class="col ms-auto py-3">
+                                                        <input class="form-control" type="number">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row pb-2">
+                                                <div class="row d-flex justify-content-between align-items-center col-md-6 ">
+                                                    <div class="col">
+                                                        <label>Lectures</label>
+                                                    </div>
+                                                    <div class="col ms-auto py-3">
+                                                        <input class="form-control" type="number">
+                                                    </div>
+                                                </div>
+                                                <div class="row d-flex justify-content-between align-items-center col-md-6 ">
+                                                    <div class="col">
+                                                        <label>Lectures</label>
+                                                    </div>
+                                                    <div class="col ms-auto py-3">
+                                                        <input class="form-control" type="number">
+                                                    </div>
+                                                </div>
+                                            </div>                     
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </form>
+                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                        </div>
                     </div>
                 </div>
             </div>
