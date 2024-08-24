234e168d73ea760dce30c9d6ed807b54a7b441ef
kusaljayawardhana <e20179@eng.pdn.ac.lk>
2024-08-24 19:15:18 +0530

    Create course model,controller and routes

---
+++ routes/backend/courses.php
@@ -0,0 +1,55 @@
+<?php
+
+use Tabuna\Breadcrumbs\Trail;
+use App\Http\Controllers\Backend\CourseController;
+
+Route::group([], function () {
+
+    // Index
+    Route::get('/courses', function () {
+        return view('backend.courses.index');
+    })->name('courses.index')
+        ->breadcrumbs(function (Trail $trail) {
+            $trail->push(__('Home'), route('dashboard.home'))
+                ->push(__('Courses'), route('courses.index'));
+        });
+
+    // Create
+    Route::get('courses/create', [CourseController::class, 'create'])
+        ->name('courses.create')
+        ->breadcrumbs(function (Trail $trail) {
+            $trail->push(__('Home'), route('dashboard.home'))
+                ->push(__('Courses'), route('courses.index'))
+                ->push(__('Create'));
+        });
+
+    // Store
+    Route::post('courses/', [CourseController::class, 'store'])
+        ->name('courses.store');
+
+    // Edit
+    Route::get('courses/edit/{course}', [CourseController::class, 'edit'])
+        ->name('courses.edit')
+        ->breadcrumbs(function (Trail $trail) {
+            $trail->push(__('Home'), route('dashboard.home'))
+                ->push(__('Courses'), route('courses.index'))
+                ->push(__('Edit'));
+        });
+
+    // Update
+    Route::put('courses/{course}', [CourseController::class, 'update'])
+        ->name('courses.update');
+
+    // Delete
+    Route::get('courses/delete/{course}', [CourseController::class, 'delete'])
+        ->name('courses.delete')
+        ->breadcrumbs(function (Trail $trail) {
+            $trail->push(__('Home'), route('dashboard.home'))
+                ->push(__('Courses'), route('courses.index'))
+                ->push(__('Delete'));
+        });
+
+    // Destroy
+    Route::delete('courses/{course}', [CourseController::class, 'destroy'])
+        ->name('courses.destroy');
+});
