61c97578c0cd9f8e921c0359238d0949bdbd5d51
NuwanJ <nuwanjaliyagoda@gmail.com>
2024-09-05 09:19:14 +0530

    Created the missing migration file again

---
+++ database/migrations/2024_08_23_165504_create_semesters_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Domains\Semester\Models\Semester;
+
+class CreateSemestersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('semesters', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->enum('version', array_keys(Semester::getVersions()));
+            $table->enum('academic_program', array_keys(Semester::getAcademicPrograms()));
+            $table->text('description')->nullable();
+            $table->string('url', 200)->unique();
+            $table->timestamps(); // This will create `created_at` and `updated_at` fields automatically
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('semesters');
+    }
+}
