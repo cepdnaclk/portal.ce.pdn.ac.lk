e24d1470843c963556cb720fb1ea3c03aa3a8917
NuwanJ <nuwanjaliyagoda@gmail.com>
2024-09-08 23:34:53 +0530

    Fixing unit tests

--- app/Domains/Auth/Http/Controllers/Frontend/Auth/RegisterController.php
+++ app/Domains/Auth/Http/Controllers/Frontend/Auth/RegisterController.php
@@ -75,7 +75,7 @@ class RegisterController
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:100'],
-            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users'),, new ValidateAsInternalEmail()],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users'), new ValidateAsInternalEmail()],
             'password' => array_merge(['max:100'], PasswordRules::register($data['email'] ?? null)),
             'terms' => ['required', 'in:1'],
             'g-recaptcha-response' => ['required_if:captcha_status,true', new Captcha],
