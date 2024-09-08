8d1d94d768d8dff0b3f84fa67fcf9469330cb384
Supun Dulara <e20087@eng.pdn.ac.lk>
2024-09-08 14:12:29 +0530

    updated semester url

--- resources/views/backend/semesters/create.blade.php
+++ resources/views/backend/semesters/create.blade.php
@@ -94,7 +94,7 @@
                     {!! Form::label('url', 'URL', ['class' => 'col-md-2 col-form-label']) !!}
                     <div class="col-md-10">
                         <div class="d-inline-flex align-items-center flex-nowrap w-100">
-                            <span class="me-2" id="url_hint">https://www.ce.pdn.ac.lk/courses/academic_program/&nbsp;</span>
+                            <span class="me-2" id="url_hint">https://www.ce.pdn.ac.lk/courses/{academic_program}/&nbsp;</span>
                             <span class="flex-grow-1"> 
                                 {!! Form::text('url', old('url', ''), ['class' => 'form-control', 'required' => true]) !!}
                             </span>
