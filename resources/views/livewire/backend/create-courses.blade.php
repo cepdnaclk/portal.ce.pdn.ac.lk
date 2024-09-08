59ce97881a38129b3e3ad264a8e1f35ba14d5e6b
Hasan10100 <150885485+Hasan10100@users.noreply.github.com>
2024-09-08 08:25:29 +0530

    Prevent text wrapping

--- resources/views/livewire/backend/create-courses.blade.php
+++ resources/views/livewire/backend/create-courses.blade.php
@@ -124,7 +124,7 @@
                                         @error('content') <div class="text-danger">{{ $message }}</div> @enderror
                                     </div>
 
-                                    <div class="div my-3 mx-3">
+                                    <div class="div my-3">
                                         <div class="row">
                                             <div class="col-md-6">
                                                 <div class=" py-2 px-3 my-2" style="border: 1px solid rgb(207, 207, 207); border-radius:5px">
@@ -190,7 +190,7 @@
                                                     <hr>
                                                     <div class="row pb-2">
                                                         <div class="row d-flex align-items-center">
-                                                            <div class="col-md-3 col-3 ">
+                                                            <div class="col-md-3 col-3">
                                                                 <label>Practicles</label>
                                                             </div>
                                                             <div class="input-group mb-3 col ms-3">
@@ -199,7 +199,7 @@
                                                             </div>
                                                         </div>
                                                         <div class="row d-flex align-items-center">
-                                                            <div class="col-md-3 col-3 ">
+                                                            <div class="col-md-3 col-3">
                                                                 <label>Project</label>
                                                             </div>
                                                             <div class="input-group mb-3 col ms-3">
@@ -208,8 +208,8 @@
                                                             </div>
                                                         </div>
                                                         <div class="row d-flex align-items-center">
-                                                            <div class="col-md-3 col-3 ">
-                                                                <label>Mid-Exam</label>
+                                                            <div class="col-md-3 col-3">
+                                                                <label style="white-space: nowrap;">Mid-Exam</label>
                                                             </div>
                                                             <div class="input-group mb-3 col ms-3">
                                                                 <input type="number" class="form-control" aria-describedby="basic-addon2">
@@ -218,7 +218,7 @@
                                                         </div>
                                                         <div class="row d-flex align-items-center">
                                                             <div class="col-md-3 col-3 ">
-                                                                <label>End-Exam</label>
+                                                                <label style="white-space: nowrap;">End-Exam</label>
                                                             </div>
                                                             <div class="input-group mb-3 col ms-3">
                                                                 <input type="number" class="form-control" aria-describedby="basic-addon2">
