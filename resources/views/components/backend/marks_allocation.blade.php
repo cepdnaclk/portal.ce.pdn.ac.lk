4ee359a74a7ae60d5d004c364dd7632c13e80926
Hasan10100 <150885485+Hasan10100@users.noreply.github.com>
2024-09-08 08:41:09 +0530

    Create blade files & connect them

---
+++ resources/views/components/backend/marks_allocation.blade.php
@@ -0,0 +1,55 @@
+<div class="col-md-6" >
+    <div class="py-2 px-3 my-2" style="border: 1px solid rgb(207, 207, 207); border-radius:5px">
+        <div class="div pt-3">
+            <label for="drop1">Marks Allocation</label>
+        </div>
+        <hr>
+        <div class="row pb-2">
+            <div class="row d-flex align-items-center">
+                <div class="col-md-3 col-3">
+                    <label>Practicles</label>
+                </div>
+                <div class="input-group mb-3 col ms-3">
+                    <input type="number" class="form-control" aria-describedby="basic-addon2">
+                    <span class="input-group-text" id="basic-addon2">%</span>
+                </div>
+            </div>
+            <div class="row d-flex align-items-center">
+                <div class="col-md-3 col-3">
+                    <label>Project</label>
+                </div>
+                <div class="input-group mb-3 col ms-3">
+                    <input type="number" class="form-control" aria-describedby="basic-addon2">
+                    <span class="input-group-text" id="basic-addon2">%</span>
+                </div>
+            </div>
+            <div class="row d-flex align-items-center">
+                <div class="col-md-3 col-3">
+                    <label style="white-space: nowrap;">Mid-Exam</label>
+                </div>
+                <div class="input-group mb-3 col ms-3">
+                    <input type="number" class="form-control" aria-describedby="basic-addon2">
+                    <span class="input-group-text" id="basic-addon2">%</span>
+                </div>
+            </div>
+            <div class="row d-flex align-items-center">
+                <div class="col-md-3 col-3 ">
+                    <label style="white-space: nowrap;">End-Exam</label>
+                </div>
+                <div class="input-group mb-3 col ms-3">
+                    <input type="number" class="form-control" aria-describedby="basic-addon2">
+                    <span class="input-group-text" id="basic-addon2">%</span>
+                </div>
+            </div>
+            <div class="row d-flex align-items-center">
+                <div class="col-md-3 col-3 ">
+                    <label>Total</label>
+                </div>
+                <div class="input-group mb-3 col ms-3">
+                    <input type="number" class="form-control" aria-describedby="basic-addon2">
+                    <span class="input-group-text" id="basic-addon2">%</span>
+                </div>
+            </div>
+        </div>
+    </div>                          
+</div>
