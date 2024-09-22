12cf17e68ec2b22a6a275c7a6172095c47e21652
NuwanJ <nuwanjaliyagoda@gmail.com>
2024-09-22 10:30:22 +0530

    Maintenance mode scripts

---
+++ scripts/deploy-prod.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+echo "Running: Down the site for maintenance"
+php artisan down --refresh=30 --render='errors::503'
+
+echo "Running: Update the branch with latest"
+git pull
+
+echo "Running: Unit test"
+php artisan test
+
+echo "Running: composer install in prod-mode"
+composer install --optimize-autoloader --no-dev
+
+echo "Running: pnpm install"
+pnpm install
+
+echo "Running: pnpm run prod"
+pnpm run prod
+
+echo "Running: Optimizing the app with caching"
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+echo "Running: Setting permissions"
+sudo chown -R www-data:www-data ./
+sudo find ./ -type f -exec chmod 644 {} \;
+sudo find ./ -type d -exec chmod 755 {} \;
+
+echo "Running: Restarting the queue"
+php artisan queue:restart
+
+echo "Running: Disable the maintenance mode"
+php artisan up
