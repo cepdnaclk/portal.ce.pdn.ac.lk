aba2d704eb998d8d33111cac827cd897fa814223
NuwanJ <nuwanjaliyagoda@gmail.com>
2024-10-13 00:25:21 +0530

    Cleanup

--- resources/views/backend/announcements/enabled_toggle.blade.php
+++
@@ -1,7 +0,0 @@
-@if ($announcement->enabled)
-    <i class="fas fa-toggle-on fa-2x" style="color: #0ca678; cursor: pointer;"
-        wire:click="toggleEnable({{ $announcement->id }})"></i>
-@else
-    <i class="fas fa-toggle-on fa-2x fa-rotate-180" style="color: #3c4b64; cursor: pointer;"
-        wire:click="toggleEnable({{ $announcement->id }})"></i>
-@endif
