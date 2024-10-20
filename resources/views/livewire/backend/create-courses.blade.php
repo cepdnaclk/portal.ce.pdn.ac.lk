94d5775169b74ce0698ecc9a3289d25e68834865
Nuwan Jaliyagoda <NuwanJ@users.noreply.github.com>
2024-10-20 22:43:05 +0530

    [CR] add a new field teaching methods into courses table #162 (#190)

--- resources/views/livewire/backend/create-courses.blade.php
+++ resources/views/livewire/backend/create-courses.blade.php
@@ -23,10 +23,12 @@
                             <h5 class="card-title">Basics</h5>
                             <div class="basics">
                                 <div class="row" id="row1">
+
+                                    {{-- Academic Program --}}
                                     <div class="col-12 col-sm-6 py-2">
                                         <div class="col ps-0">
                                             <label for="drop1">
-                                                Academic Program
+                                                Academic Program*
                                             </label>
                                         </div>
                                         <select class="form-select" wire:model="academicProgram">
@@ -40,9 +42,11 @@
                                             <div class="text-danger">{{ $message }}</div>
                                         @enderror
                                     </div>
+
+                                    {{-- Curriculum --}}
                                     <div class="col-12 col-sm-6 py-2">
                                         <div class="col ps-0">
-                                            <label for="drop1">Curriculum</label>
+                                            <label for="drop1">Curriculum*</label>
                                         </div>
                                         <select class="form-select" wire:model="version">
                                             <option style="display:none" selected></option>
@@ -54,9 +58,11 @@
                                             <div class="text-danger">{{ $message }}</div>
                                         @enderror
                                     </div>
+
+                                    {{-- Semester --}}
                                     <div class="col-12 py-2">
                                         <div class="col ps-0">
-                                            <label for="drop1">Semester</label>
+                                            <label for="drop1">Semester*</label>
                                         </div>
                                         <select class="form-select" wire:model="semester">
                                             <option style="display:none" selected></option>
@@ -68,9 +74,11 @@
                                             <div class="text-danger">{{ $message }}</div>
                                         @enderror
                                     </div>
+
+                                    {{-- Course Code --}}
                                     <div class="col-12 col-sm-3 py-2">
                                         <div class="col ps-0">
-                                            <label>Code</label>
+                                            <label>Code*</label>
                                         </div>
                                         <div class="input-group">
                                             <input type="text" class="form-control" wire:model.lazy = "code">
@@ -79,9 +87,11 @@
                                             <span class="text-danger">{{ $message }}</span>
                                         @enderror
                                     </div>
+
+                                    {{-- Course Name --}}
                                     <div class="col-12 col-sm-9 py-2">
                                         <div class="col ps-0">
-                                            <label>Name</label>
+                                            <label>Name*</label>
                                         </div>
                                         <div class="input-group">
                                             <input type="text" class="form-control" wire:model.lazy = "name">
@@ -92,9 +102,10 @@
                                     </div>
                                 </div>
                                 <div class="row" id="row2">
+                                    {{-- Course Type --}}
                                     <div class="col-12 col-sm-6 py-2">
                                         <div class="col ps-0">
-                                            <label for="drop1">Type</label>
+                                            <label for="drop1">Type*</label>
                                         </div>
                                         <select class="form-select" wire:model="type">
                                             <option style="display:none" selected></option>
@@ -106,9 +117,11 @@
                                             <div class="text-danger">{{ $message }}</div>
                                         @enderror
                                     </div>
+
+                                    {{-- Credits --}}
                                     <div class="col-12 col-sm-6 py-2">
                                         <div class="col ps-0">
-                                            <label>Credits</label>
+                                            <label>Credits*</label>
                                         </div>
                                         <div class="input-group">
                                             <input type="text" class="form-control" wire:model.lazy ="credits">
@@ -117,6 +130,22 @@
                                             <span class="text-danger">{{ $message }}</span>
                                         @enderror
                                     </div>
+
+                                    {{-- Teaching Methods --}}
+                                    <div class="col-12 py-2">
+                                        <div class="col ps-0">
+                                            <label>Teaching Methods</label>
+                                        </div>
+                                        <div class="input-group">
+                                            <input type="text" class="form-control"
+                                                wire:model.lazy = "teaching_methods">
+                                        </div>
+                                        @error('teaching_methods')
+                                            <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+
+                                    {{-- FAQ Page --}}
                                     <div class="col-12 py-2">
                                         <div class="col ps-0">
                                             <label>FAQ page</label>
@@ -129,6 +158,8 @@
                                             <div class="text-danger">{{ $message }}</div>
                                         @enderror
                                     </div>
+
+                                    {{-- Content --}}
                                     <div class="my-2" id="contentarea">
                                         <label for="contentTextarea">Content</label>
                                         <textarea class="form-control" id="contentTextarea" wire:model.lazy = "content" rows="3"></textarea>
@@ -185,7 +216,7 @@
                         <hr>
                     </div>
 
-                    {{-- ILO --}}
+                    {{-- ILOs --}}
                     @foreach ($ilos as $key => $value)
                         <div class="mt-5">
                             @livewire('backend.item-adder', ['type' => $key, 'items' => $ilos[$key]], key("ilos-$key-adder"))
@@ -201,14 +232,15 @@
                 <div class="card-body">
                     <h5 class="card-title">Modules & References</h5>
 
+                    {{-- Modules --}}
                     <div class="pb-5">
                         <x-backend.course_module></x-backend.course_module>
                     </div>
 
+                    {{-- References --}}
                     <div class="pb-5">
                         @livewire('backend.item-adder', ['type' => 'references', 'items' => $references], key('references-adder'))
                     </div>
-
                 </div>
             </div>
         </div>
