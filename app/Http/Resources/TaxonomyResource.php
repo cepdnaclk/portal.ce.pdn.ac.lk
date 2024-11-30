34cb7286d39984b9f68031632accf0217e3a78bd
NuwanJ <nuwanjaliyagoda@gmail.com>
2024-11-30 20:24:40 +0530

    Small fix

--- app/Http/Resources/TaxonomyResource.php
+++ app/Http/Resources/TaxonomyResource.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Resources;
 
-use App\Domains\Auth\Models\User;
+// use App\Domains\Auth\Models\User;
 use App\Domains\Taxonomy\Models\Taxonomy;
 use App\Http\Resources\TaxonomyTermResource;
 use Illuminate\Http\Resources\Json\JsonResource;
