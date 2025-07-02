0baa81a4bb43b35e52dff4acf1b1cef67d8766a8
NuwanJ <nuwanjaliyagoda@gmail.com>
2025-07-02 08:00:53 +0530

    Test case fix

--- tests/Feature/Rules/SlugValidationTest.php
+++ tests/Feature/Rules/SlugValidationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Rules\Slug;
 use Validator;
 use Tests\TestCase;
 
@@ -10,7 +11,7 @@ class SlugValidationTest extends TestCase
     /** @test */
     public function test_it_accepts_valid_slugs(): void
     {
-        $rule = ['slug' => 'slug'];
+        $rule = ['slug' => [new Slug()]];
         $this->assertTrue(Validator::make(['slug' => 'hello-world'], $rule)->passes());
         $this->assertTrue(Validator::make(['slug' => 'foo-123-bar'], $rule)->passes());
     }
@@ -18,8 +19,11 @@ class SlugValidationTest extends TestCase
     /** @test */
     public function test_it_rejects_invalid_slugs(): void
     {
-        $rule = ['slug' => 'slug'];
+        $rule = ['slug' => [new Slug()]];
         $this->assertFalse(Validator::make(['slug' => 'Hello'], $rule)->passes());
+        $this->assertFalse(Validator::make(['slug' => '--foo'], $rule)->passes());
+        $this->assertFalse(Validator::make(['slug' => 'bar--'], $rule)->passes());
         $this->assertFalse(Validator::make(['slug' => 'foo--bar'], $rule)->passes());
+        $this->assertFalse(Validator::make(['slug' => 'foo*'], $rule)->passes());
     }
 }
