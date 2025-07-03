e34036f669e8e28003739242d7042d5fc1efbd47
Nuwan Jaliyagoda <NuwanJ@users.noreply.github.com>
2025-07-03 10:09:45 +0530

    Text editor enhancements #311 (#312)

--- resources/views/livewire/backend/richtext-editor-component.blade.php
+++ resources/views/livewire/backend/richtext-editor-component.blade.php
@@ -1,19 +1,49 @@
 <textarea name="{{ $name }}" id="{{ $name }}" rows="10" class="form-control">{!! $value !!}</textarea>
 
 @push('after-scripts')
+    <script src="{{ asset('js/tinymce/tinymce.min.js') }}" referrerpolicy="origin"></script>
     <script>
-        // WYSIWYG Editor
-        ClassicEditor
-            .create(document.querySelector('#{{ $name }}'), {
-                toolbar: [
-                    'heading', '|',
-                    'bold', 'italic', 'link', '|',
-                    'bulletedList', 'numberedList', 'blockQuote', '|',
-                    'undo', 'redo',
-                ],
-            })
-            .catch(error => {
-                console.error(error);
-            });
+        tinymce.init({
+            selector: 'textarea#{{ $name }}',
+            plugins: 'code table lists link preview',
+            license_key: 'gpl',
+            menubar: false,
+            height: 500,
+            branding: false,
+            toolbar: [{
+                    name: 'history',
+                    items: ['undo', 'redo']
+                },
+                {
+                    name: 'styles',
+                    items: ['styles']
+                },
+                {
+                    name: 'formatting',
+                    items: ['bold', 'italic', 'underline', 'strikethrough', 'subscript', 'superscript']
+                },
+                {
+                    name: 'links',
+                    items: ['link']
+                },
+                {
+                    name: 'alignment',
+                    items: ['alignleft', 'aligncenter', 'alignright', 'alignjustify']
+                },
+                {
+                    name: 'lists',
+                    items: ['bullist', 'numlist']
+                },
+                {
+                    name: 'indentation',
+                    items: ['outdent', 'indent']
+                },
+                {
+                    name: "tools",
+                    items: ['table', 'hr', 'removeformat', 'code', 'preview']
+                }
+            ],
+            toolbar_mode: 'sliding'
+        });
     </script>
 @endpush
