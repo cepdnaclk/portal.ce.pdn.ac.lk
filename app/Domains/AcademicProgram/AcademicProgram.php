3dfe71d5b2bd6bac91eac8cea4ff682b725eee09
NuwanJ <nuwanjaliyagoda@gmail.com>
2025-07-19 11:55:22 +0530

    Implement caching for academic program versions and course types

--- app/Domains/AcademicProgram/AcademicProgram.php
+++ app/Domains/AcademicProgram/AcademicProgram.php
@@ -2,12 +2,15 @@
 
 namespace App\Domains\AcademicProgram;
 
+use App\Domains\Taxonomy\Models\TaxonomyTerm;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AcademicProgram extends Model
 {
     use HasFactory;
+
+    const CACHE_DURATION = 3600; // Cache duration in seconds (1 hour)
 
     public static function getAcademicPrograms(): array
     {
@@ -17,18 +20,34 @@ class AcademicProgram extends Model
         ];
     }
 
+    /*
+    * Get the versions of the academic programs.
+    * This is used to get the versions of the academic programs
+    *
+    * @param string|null $academicProgram
+    * @return array
+    */
     public static function getVersions($academicProgram = null): array
     {
-        // TODO integrate with Taxonomies
-        $academicPrograms = [
-            'undergraduate' => [
-                1 => 'Current Curriculum',
-                2 => 'Curriculum - Effective from E22'
-            ],
-            'postgraduate' => [
-                3 => 'Current Curriculum - PG',
-            ]
-        ];
+        $cacheKey = 'academic_program_versions';
+        $academicPrograms = cache()->remember($cacheKey, now()->addSeconds(self::CACHE_DURATION), function () {
+            $undergraduate = TaxonomyTerm::where('code', 'academic_program_undergraduate')->firstOrFail();
+            $postgraduate = TaxonomyTerm::where('code', 'academic_program_postgraduate')->firstOrFail();
+
+            $academicPrograms = [
+                'undergraduate' => [],
+                'postgraduate' => []
+            ];
+
+            foreach (['undergraduate' => $undergraduate, 'postgraduate' => $postgraduate] as $type => $program) {
+                foreach ($program->children as $child) {
+                    $code = (int) $child->getFormattedMetadata('key');
+                    $academicPrograms[$type][$code] = $child->name;
+                }
+            }
+
+            return $academicPrograms;
+        });
 
         if ($academicProgram == null) {
             $allAcademicPrograms = [];
@@ -48,13 +67,24 @@ class AcademicProgram extends Model
         return $this::getVersions($this->academic_program)[$this->version] ?? null;
     }
 
+    /*
+    * Get the course types for the academic program.
+    * This is used to get the course types such as Foundation, Core, GE, etc.
+    *
+    * @return array
+    */
     public static function getTypes(): array
     {
-        return [
-            'Foundation' => 'Foundation',
-            'Core' => 'Core',
-            'GE' => 'General Elective',
-            'TE' => 'Technical Elective'
-        ];
+        $cacheKey = 'academic_program_course_types';
+        $types = cache()->remember($cacheKey, now()->addSeconds(self::CACHE_DURATION), function () {
+            $courseTypes = TaxonomyTerm::where('code', 'course_types')->firstOrFail();
+            $types = [];
+            foreach ($courseTypes->children as $child) {
+                $types[$child->getFormattedMetadata('key')] = $child->name;
+            }
+            return $types;
+        });
+
+        return $types;
     }
 }
