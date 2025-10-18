bebb9204f8e9c385c1e7b5ea9be5e2d3f358ce56
NuwanJ <nuwanjaliyagoda@gmail.com>
2025-10-18 22:03:33 +0530

    Remove comments

--- resources/views/livewire/backend/taxonomy-list-item-manager.blade.php
+++ resources/views/livewire/backend/taxonomy-list-item-manager.blade.php
@@ -227,8 +227,7 @@
                    </div>
                    <div class="modal-footer">
                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                       <button type="button" class="btn btn-primary" {{-- data-bs-dismiss="modal" --}}
-                           x-on:click="handleSave()">Save Changes</button>
+                       <button type="button" class="btn btn-primary" x-on:click="handleSave()">Save Changes</button>
                    </div>
                </div>
            </div>
