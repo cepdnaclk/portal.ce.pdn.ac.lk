d24ab02d7ed0cebe7dd36a058e0bb176e0b44bc2
NuwanJ <nuwanjaliyagoda@gmail.com>
2026-01-08 19:08:16 +0530

    refactor: Replace custom buttons with standard HTML for tenant action buttons

--- resources/views/backend/tenant/includes/actions.blade.php
+++ resources/views/backend/tenant/includes/actions.blade.php
@@ -1,4 +1,13 @@
-<x-utils.edit-button :href="route('dashboard.tenants.edit', $model)" />
-@if ($assignmentsCount === 0)
-    <x-utils.delete-button :href="route('dashboard.tenants.destroy', $model)" />
-@endif
+<div class="btn-group" role="group" aria-label="{{ __('Actions') }}">
+    {{-- Edit --}}
+    <a href="{{ route('dashboard.tenants.edit', $model) }}" class="btn btn-sm btn-warning">
+        <i class="fa fa-pencil" title="{{ __('Edit') }}"></i>
+    </a>
+
+    @if ($assignmentsCount === 0)
+        {{-- Delete --}}
+        <a href="{{ route('dashboard.tenants.destroy', $model) }}" class="btn btn-sm btn-danger">
+            <i class="fa fa-trash" title="{{ __('Delete') }}"></i>
+        </a>
+    @endif
+</div>
