822645779e4d7c45d04dd3400b82d96632b30541
NuwanJ <nuwanjaliyagoda@gmail.com>
2026-03-04 16:11:39 +0530

    fix: Update email sender address and enhance footer with support contact information

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
   'from' => [
-    'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-    'name' => env('MAIL_FROM_NAME', 'Example'),
+    'address' => env('MAIL_FROM_ADDRESS', 'portal@ce.pdn.ac.lk'),
+    'name' => env('MAIL_FROM_NAME', 'CE Portal'),
   ],
 
   /*
--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -3,6 +3,11 @@
         <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
             <tr>
                 <td class="content-cell" align="center">
+                    This is auto-generated email. Please do not reply to the sender. If you have any concern about the
+                    email, or if you feel you are not the correct person, please contact us through
+                    <a
+                        href="mailto:{{ config('email-service.support_email') }}">{{ config('email-service.support_email') }}</a>.
+                    <hr />
                     {{ Illuminate\Mail\Markdown::parse($slot) }}
                 </td>
             </tr>
